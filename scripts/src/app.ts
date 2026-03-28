4885ad5e01b494f86cf7f6dd8720f96cff75f832
endometriosisandme <endometriosisandme@gmail.com>
2026-03-28 12:56:07 -0400

    Set up Express app with pino-http middleware

--- scripts/src/app.ts
+++ scripts/src/app.ts
@@ -1,1 +1,12 @@
-import pinoHttp from "pino-http";
+import express, { Request, Response } from "express";
+import pinoHttp = require("pino-http"); // ✅ THIS avoids the Vercel issue
+
+const app = express();
+
+app.use(pinoHttp());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("API is running");
+});
+
+export default app;
